298266c1f39b2c530d580e79e0cc57cfb31f8fa9
Sam Kravitz <samkravitz99@gmail.com>
2022-08-10 21:11:18 -0400

    Implement generic tree base class

---
+++ util/tree_node.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include <functional>
+#include <memory>
+#include <vector>
+
+namespace util
+{
+template<class T>
+class TreeNode
+{
+public:
+	TreeNode() = default;
+	~TreeNode()  = default;
+
+	void add_child(std::shared_ptr<T> node) { children.push_back(node); }
+
+	void preorder(std::function<void(std::shared_ptr<T>)> f)
+	{
+		for (auto child : children)
+		{
+			child->preorder(f);
+			f(child);
+		}
+	}
+
+	void for_each_child(std::function<void(std::shared_ptr<T>)> f)
+	{
+		for (auto child : children)
+			f(child);
+	}
+
+	std::shared_ptr<T> last_child()
+	{
+		if (!has_children())
+			return nullptr;
+
+		return children.back();
+	}
+
+	bool has_children() const { return children.size() != 0; }
+
+	std::size_t size() const
+	{
+		std::size_t count = 1;
+		for (auto child : children)
+			count += child->size();
+
+		return count;
+	}
+
+protected:
+	std::vector<std::shared_ptr<T>> children;
+};
+}
