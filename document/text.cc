91e728f091804ef24cb882a2bbf7eb89697f716f
Sam Kravitz <samkravitz99@gmail.com>
2022-08-18 20:52:05 -0400

    [document] Add method to find a text node that consists only of whitespace

--- document/text.cc
+++ document/text.cc
@@ -1,9 +1,11 @@
 #include "text.h"
 
+#include <algorithm>
+#include <cctype>
 #include <iostream>
 
 Text::Text(std::string text) :
-	m_text(text)
+    m_text(text)
 { }
 
 Text::Text(char c)
@@ -25,4 +27,9 @@ void Text::print(int depth) const
 
 	for (auto child : children)
 		child->print(depth + 1);
+}
+
+bool Text::whitespace_only() const
+{
+	return std::all_of(m_text.begin(), m_text.end(), [](char c) { return std::isspace(c); });
 }
--- document/text.h
+++ document/text.h
@@ -10,11 +10,12 @@ public:
 	Text(std::string);
 	Text(char);
 
-	void append(char);
-	void print(int depth = 0) const ;
-
 	inline NodeType type() const { return NodeType::Text; }
 	inline std::string text() const { return m_text; }
+
+	void append(char);
+	void print(int depth = 0) const;
+	bool whitespace_only() const;
 
 private:
 	std::string m_text;
