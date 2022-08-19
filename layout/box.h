4240e0211cc3b61c3e008cb9f793e6ebd58648aa
Sam Kravitz <samkravitz99@gmail.com>
2022-08-18 20:50:45 -0400

    [layout] Move Edges to its own implementation file

--- layout/box.h
+++ layout/box.h
@@ -4,14 +4,6 @@
 
 namespace layout
 {
-struct Edges
-{
-	int left = 0;
-	int right = 0;
-	int top = 0;
-	int bottom = 0;
-};
-
 struct Box
 {
 	// content box
---
+++ layout/edges.h
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace layout
+{
+struct Edges
+{
+	int left = 0;
+	int right = 0;
+	int top = 0;
+	int bottom = 0;
+};
+}
