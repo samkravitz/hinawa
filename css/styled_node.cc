3fb4870c77d47a4765d8e96fbf0d3f2d0ab76e1a
Sam Kravitz <samkravitz99@gmail.com>
2022-12-16 18:06:16 -0500

    [css] Remove null check in lookup()

--- css/styled_node.cc
+++ css/styled_node.cc
@@ -72,9 +72,6 @@ StyledNode::StyledNode(std::shared_ptr<Node> node,
 
 Value *StyledNode::lookup(std::string property_name, Value *const fallback)
 {
-	if (!this)
-		return fallback;
-
 	if (m_values.find(property_name) != m_values.end())
 		return m_values[property_name];
 
@@ -83,9 +80,6 @@ Value *StyledNode::lookup(std::string property_name, Value *const fallback)
 
 Value *StyledNode::lookup(std::string property_name1, std::string property_name2, Value *const fallback)
 {
-	if (!this)
-		return fallback;
-
 	if (m_values.find(property_name1) != m_values.end())
 		return m_values[property_name1];
 
