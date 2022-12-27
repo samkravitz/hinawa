eddc39952b3f8429e4ddefaff804ecc956e52873
Sam Kravitz <samkravitz99@gmail.com>
2022-12-26 21:01:18 -0500

    Can load pages with relative paths again ./hinawa ../index.html

--- main.cc
+++ main.cc
@@ -1,15 +1,22 @@
+#include <filesystem>
 #include <string>
+
+namespace fs = std::filesystem;
 
 #include "browser/browser.h"
 
-static const char *homepage_url = "file:///home/sam/projects/hinawa/data/homepage.html";
+static const char *homepage_url = "../data/homepage.html";
 
 int main(int argc, char **argv)
 {
 	std::string html_file = homepage_url;
 	if (argc > 1)
-		html_file = argv[1];
-
-	auto hinawa = browser::Browser(html_file);
+	{
+		if (fs::exists(argv[1]))
+			html_file = argv[1];
+	}
+	
+	auto path = fs::canonical(html_file);
+	auto hinawa = browser::Browser("file://" + std::string(path.c_str()));
 	return 0;
 }
