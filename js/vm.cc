fa40f2c18a649d47d7908bbb69e30be78ba63584
Sam Kravitz <samkravitz99@gmail.com>
2023-01-18 16:35:01 -0500

    [js] Format vm.cc

--- js/vm.cc
+++ js/vm.cc
@@ -8,8 +8,8 @@
 #include <fmt/format.h>
 
 #include "chunk.h"
-#include "object_string.h"
 #include "object.h"
+#include "object_string.h"
 #include "opcode.h"
 
 namespace js
@@ -26,8 +26,7 @@ Vm::Vm(bool headless)
 	global->set("window", Value(global));
 
 	auto *console = new Object();
-	console->set_native("log", [](auto &vm, const auto &argv) -> Value
-	{
+	console->set_native("log", [](auto &vm, const auto &argv) -> Value {
 		if (argv.empty())
 			return {};
 
@@ -37,17 +36,17 @@ Vm::Vm(bool headless)
 			if (i != argv.size() - 1)
 				std::cout << " ";
 		}
-		
+
 		std::cout << "\n";
 		return {};
 	});
-	
+
 	global->set("console", Value(console));
 }
 
 bool Vm::run(Function f)
 {
-	auto cf = CallFrame { f, 0 };
+	auto cf = CallFrame{ f, 0 };
 	frames.push(cf);
 	push(Value(&f));
 
@@ -65,7 +64,7 @@ bool Vm::run(Function f)
 
 				if (frames.empty())
 					return true;
-				
+
 				auto diff = stack.size() - frame.base;
 				for (uint i = 0; i < diff; i++)
 					pop();
@@ -73,7 +72,7 @@ bool Vm::run(Function f)
 				push(result);
 				break;
 			}
-			
+
 			case OP_CONSTANT:
 			{
 				auto constant = read_constant();
@@ -92,42 +91,42 @@ bool Vm::run(Function f)
 			case OP_SUBTRACT:
 				binary_op(Operator::Minus);
 				break;
-			
+
 			case OP_MULTIPLY:
 				binary_op(Operator::Star);
 				break;
-			
+
 			case OP_DIVIDE:
 				binary_op(Operator::Slash);
 				break;
-			
+
 			case OP_MOD:
 				binary_op(Operator::Mod);
 				break;
-			
+
 			case OP_NULL:
-				push(Value(Value::Type::Null));
+				push(Value(nullptr));
 				break;
 
 			case OP_UNDEFINED:
-				push(Value());
+				push({});
 				break;
-			
+
 			case OP_TRUE:
 				push(Value(true));
 				break;
-			
+
 			case OP_FALSE:
 				push(Value(false));
 				break;
-			
+
 			case OP_NOT:
 			{
 				auto value = pop();
 				push(Value(value.is_falsy()));
 				break;
 			}
-			
+
 			case OP_EQUAL:
 			{
 				auto b = pop();
@@ -135,15 +134,15 @@ bool Vm::run(Function f)
 				push(Value(a == b));
 				break;
 			}
-			
+
 			case OP_GREATER:
 				binary_op(Operator::GreaterThan);
 				break;
-			
+
 			case OP_LESS:
 				binary_op(Operator::LessThan);
 				break;
-			
+
 			case OP_LOGICAL_AND:
 				binary_op(Operator::AmpAmp);
 				break;
@@ -151,7 +150,7 @@ bool Vm::run(Function f)
 			case OP_LOGICAL_OR:
 				binary_op(Operator::PipePipe);
 				break;
-			
+
 			case OP_BITWISE_AND:
 				binary_op(Operator::Amp);
 				break;
@@ -159,7 +158,7 @@ bool Vm::run(Function f)
 			case OP_BITWISE_OR:
 				binary_op(Operator::Pipe);
 				break;
-			
+
 			case OP_POP:
 				pop();
 				break;
@@ -220,7 +219,7 @@ bool Vm::run(Function f)
 				auto offset = read_short();
 				if (peek().is_falsy())
 					frames.top().ip += offset;
-				
+
 				break;
 			}
 
@@ -254,14 +253,14 @@ bool Vm::run(Function f)
 					auto result = callee.as_native()->call(*this, argv);
 					for (int i = 0; i < num_args + 1; i++)
 						pop();
-					
+
 					push(result);
 				}
 
 				else if (callee.is_function())
 				{
 					auto base = static_cast<uint>(stack.size() - num_args - 1);
-					auto cf = CallFrame { *callee.as_function(), base };
+					auto cf = CallFrame{ *callee.as_function(), base };
 					frames.push(cf);
 				}
 
@@ -269,7 +268,7 @@ bool Vm::run(Function f)
 				{
 					assert(!"Tried to call an uncallable object");
 				}
-				
+
 				break;
 			}
 
@@ -279,7 +278,7 @@ bool Vm::run(Function f)
 				std::vector<Value> *array = new std::vector<Value>;
 				while (num_elements--)
 					array->push_back(pop());
-				
+
 				std::reverse(array->begin(), array->end());
 				push(Value(array));
 				break;
@@ -309,7 +308,8 @@ bool Vm::run(Function f)
 				int idx = (int) index.as_number();
 				if (idx < 0 || idx >= (int) array->size())
 				{
-					if (!runtime_error(fmt::format("Error: array index {} out of bounds (length {})", idx, array->size())))
+					if (!runtime_error(
+					        fmt::format("Error: array index {} out of bounds (length {})", idx, array->size())))
 						return false;
 					break;
 				}
@@ -343,7 +343,8 @@ bool Vm::run(Function f)
 				int idx = (int) index.as_number();
 				if (idx < 0 || idx >= (int) array->size())
 				{
-					if (!runtime_error(fmt::format("Error: array index {} out of bounds (length {})", idx, array->size())))
+					if (!runtime_error(
+					        fmt::format("Error: array index {} out of bounds (length {})", idx, array->size())))
 						return false;
 					break;
 				}
@@ -353,13 +354,13 @@ bool Vm::run(Function f)
 				break;
 			}
 
-			//case OP_CLASS:
-			//{
-			//	auto name = read_constant();
-			//	auto klass = std::make_shared<Klass>(name.as_string());
-			//	push(std::make_shared<Value>(klass));
-			//	break;
-			//}
+				//case OP_CLASS:
+				//{
+				//	auto name = read_constant();
+				//	auto klass = std::make_shared<Klass>(name.as_string());
+				//	push(std::make_shared<Value>(klass));
+				//	break;
+				//}
 
 			case OP_GET_PROPERTY:
 			{
@@ -449,13 +450,13 @@ bool Vm::run(Function f)
 				assert(!"Unknown opcode");
 		}
 
-		#ifdef DEBUG_PRINT_STACK
+#ifdef DEBUG_PRINT_STACK
 		std::printf("stack:          ");
 		std::printf("[ ");
 		for (auto &x : stack)
 			std::printf("%s ", x.to_string().c_str());
 		std::printf("]\n");
-		#endif
+#endif
 	}
 }
 
@@ -501,7 +502,7 @@ void Vm::binary_op(Operator op)
 			result = a - b;
 			push(Value(result));
 			break;
-		
+
 		case Operator::Star:
 			result = a * b;
 			push(Value(result));
@@ -516,17 +517,17 @@ void Vm::binary_op(Operator op)
 			result = (int) a % (int) b;
 			push(Value(result));
 			break;
-		
+
 		case Operator::LessThan:
 			x = a < b;
 			push(Value(x));
 			break;
-		
+
 		case Operator::GreaterThan:
 			x = a > b;
 			push(Value(x));
 			break;
-		
+
 		case Operator::Amp:
 			x = (int) a & (int) b;
 			push(Value(x));
@@ -536,17 +537,17 @@ void Vm::binary_op(Operator op)
 			x = (int) a && (int) b;
 			push(Value(x));
 			break;
-		
+
 		case Operator::Pipe:
 			x = (int) a | (int) b;
 			push(Value(x));
 			break;
-		
+
 		case Operator::PipePipe:
 			x = (int) a || (int) b;
 			push(Value(x));
 			break;
-		
+
 		default:
 			assert(!"Unreachable");
 	}
@@ -593,7 +594,7 @@ bool Vm::runtime_error(std::string const &msg)
 		fmt::print("[line {}] Uncaught {}\n", line, msg);
 		return false;
 	}
-	
+
 	auto ip = catchv.back();
 	frames.top().ip = ip;
 	return true;
