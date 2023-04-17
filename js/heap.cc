965784fb2565a9832d3f47795a909727903e0b24
Sam Kravitz <samkravitz99@gmail.com>
2023-04-17 08:24:56 -0500

    [js] Add heap module to handle allocations

---
+++ js/heap.cc
@@ -0,0 +1,4 @@
+#include "heap.h"
+
+namespace js
+{ }
---
+++ js/heap.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <utility>
+
+namespace js
+{
+class Heap
+{
+public:
+	Heap() = default;
+
+	template<class T, typename... Params>
+	T *allocate(Params &&...params)
+	{
+		return new T(std::forward<Params>(params)...);
+	}
+};
+}
