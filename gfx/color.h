37a14baea34cb5025320004ab6c1889b5fd6d745
Sam Kravitz <samkravitz99@gmail.com>
2024-02-28 10:57:06 -0600

    [gfx] Add Color::from_string

--- gfx/color.h
+++ gfx/color.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <unordered_map>
+
 #include "css/value.h"
 
 struct Color
@@ -35,6 +37,33 @@ struct Color
 	u8 g;
 	u8 b;
 	u8 a = 0xff;
+
+	static Color from_string(const std::string &color)
+	{
+		static std::unordered_map<std::string, Color> color_strings = {
+		    {"black",   Color(0,    0,    0)   },
+		    {"white",   Color(0xff, 0xff, 0xff)},
+		    {"gray",    Color(0x80, 0x80, 0x80)},
+		    {"silver",  Color(0xc0, 0xc0, 0xc0)},
+		    {"maroon",  Color(0x80, 0,    0)   },
+		    {"red",     Color(0xff, 0,    0)   },
+		    {"purple",  Color(0x80, 0,    0x80)},
+		    {"fushsia", Color(0xff, 0,    0xff)},
+		    {"green",   Color(0,    0xff, 0)   },
+		    {"lime",    Color(0,    0xff, 0)   },
+		    {"olive",   Color(0x80, 0x80, 0)   },
+		    {"yellow",  Color(0xff, 0xff, 0)   },
+		    {"navy",    Color(0,    0,    0x80)},
+		    {"blue",    Color(0,    0,    0xff)},
+		    {"teal",    Color(0,    0x80, 0x80)},
+		    {"aqua",    Color(0,    0xff, 0xff)},
+		};
+
+		if (color_strings.contains(color))
+			return color_strings[color];
+
+		return Color::BLACK;
+	}
 };
 
 inline const Color Color::BLACK(0x0, 0x0, 0x0);
