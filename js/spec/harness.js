2c01a29d328d212811427bae84bed61e278b21c6
Sam Kravitz <samkravitz99@gmail.com>
2024-10-28 15:51:53 -0500

    [js] Add ability to group tests into suites using describe()

--- js/spec/harness.js
+++ js/spec/harness.js
@@ -1,41 +1,55 @@
-const __PassedTests__ = [];
-const __FailedTests__ = [];
+const __TestResults__ = {};
+
+const defaultSuiteMessage = "__TOP_LEVEL__";
+let suiteMessage = defaultSuiteMessage;
+
+const describe = (message, callback) => {
+  suiteMessage = message;
+  if (!__TestResults__[suiteMessage]) __TestResults__[suiteMessage] = {};
+
+  try {
+    callback();
+  } catch (e) {
+    console.log("Suite " + suiteMessage + " failed to run.");
+  }
+
+  suiteMessage = defaultSuiteMessage;
+};
+
+const test = (message, callback) => {
+  if (!__TestResults__[suiteMessage]) __TestResults__[suiteMessage] = {};
+
+  const suite = __TestResults__[suiteMessage];
+
+  try {
+    callback();
+    suite[message] = {
+      passed: true
+    };
+  } catch (e) {
+    suite[message] = {
+      passed: false,
+      details: e
+    };
+  }
+};
 
 const expect = (value) => {
   return new Expector(value);
 };
 
-const test = (message, callback) => {
-  try {
-    callback();
-    __PassedTests__.push({
-      message: message
-    });
-  } catch (e) {
-    __FailedTests__.push({
-      message: message,
-      expect: e.expect,
-      got: e.got
-    });
-  }
-};
-
 function Expector(target, inverted) {
   this.target = target;
-	this.inverted = !!inverted;
+  this.inverted = !!inverted;
 
-  this.toEqual = function(value) {
+  this.toEqual = function (value) {
     if (value !== this.target) {
-      const details = {
-        expect: target,
-        got: value
-      };
-
+      const details = "Expected: " + target + " got: " + value;
       throw details;
     }
   };
 
-	this.not = function() {
-		return new Expector(this.target, !this.inverted)
-	}
+  this.not = function () {
+    return new Expector(this.target, !this.inverted);
+  };
 }
