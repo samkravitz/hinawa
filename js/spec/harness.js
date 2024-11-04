0fa097a32fd2cb57cbb50a02514dd31a0138527b
Sam Kravitz <samkravitz99@gmail.com>
2024-11-04 14:27:38 -0600

    [js] Add new matchers

--- js/spec/harness.js
+++ js/spec/harness.js
@@ -44,7 +44,35 @@ function Expector(target, inverted) {
 
   this.toEqual = function (value) {
     if (value !== this.target) {
-      const details = "Expected: " + target + " got: " + value;
+      const details = "toEqual(): Expected: " + target + " got: " + value;
+      throw details;
+    }
+  };
+
+  this.toBeTrue = function () {
+    if (this.target !== true) {
+      const details = "toBeTrue(): Expected true, got: " + this.target;
+      throw details;
+    }
+  };
+
+  this.toBeFalse = function () {
+    if (this.target !== false) {
+      const details = "toBeFalse(): Expected false, got: " + this.target;
+      throw details;
+    }
+  };
+
+  this.toBeNull = function () {
+    if (this.target !== null) {
+      const details = "toBeNull(): Expected null, got: " + this.target;
+      throw details;
+    }
+  };
+
+  this.toBeUndefined = function () {
+    if (this.target !== undefined) {
+      const details = "toBeUndefined(): Expected undefined, got: " + this.target;
       throw details;
     }
   };
